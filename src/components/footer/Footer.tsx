6ed58d5ead03ecf1f4202e4b9eb3e2ea35dabc88
sanjay <sanjayprajapati2077@kct.edu.np>
2024-09-01 17:16:44 +0545

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Sanjayadmin</span>
+      <span>© Sanjay Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
